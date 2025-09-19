4676023cc18085ebc4ad8506826567fe93760b34
ahenao@ofima.com <ahenao@ofima.com>
2025-09-19 10:20:41 -0500

    update

--- lib/pages/carrito/carrito_widget.dart
+++ lib/pages/carrito/carrito_widget.dart
@@ -7,12 +7,10 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:ui';
 import '/custom_code/actions/index.dart' as actions;
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'carrito_model.dart';
 export 'carrito_model.dart';
@@ -339,9 +337,8 @@ class _CarritoWidgetState extends State<CarritoWidget> {
                                                     color: Colors.transparent,
                                                     child:
                                                         ItemShoppingCartWidget(
-                                                      key: Key(
-                                                        'Keyarb_${articulosIndex.toString()}',
-                                                      ),
+                                                      key: ValueKey(
+                                                          '${articulosItem.codigo}-${articulosItem.bodega}'),
                                                       articulo: articulosItem,
                                                       cantidad: articulosItem
                                                           .cantidad,
